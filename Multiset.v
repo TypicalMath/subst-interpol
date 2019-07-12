b758613339ba7b7059d7f8eaa8b070f72816f4e5
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2019-07-12 18:24:52 +0430

    added set notations

--- Multiset.v
+++ Multiset.v
@@ -6,3 +6,10 @@ Notation "[ P ]" := (SingletonBag eq propeq_dec P).
 Infix ";" := munion (left associativity, at level 61).
 Coercion prop_to_singleton (P : prop) : (multiset prop) := 
   SingletonBag eq propeq_dec P.
+
+Definition In {A : Type} (a : A) (M : multiset A) : Prop :=
+  O < multiplicity M a.
+Infix "∈" := In (at level 60). (*2208*)
+Definition Subset {A : Type} (M1 M2 : multiset A) : Prop :=
+  forall a, a ∈ M1 -> a ∈ M2.
+Infix "⊆" := Subset (at level 61). (* 2286 *)
