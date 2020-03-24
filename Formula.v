4ec51dedd7d9a4b9187d64d7649fdf250d4a2033
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2020-03-24 22:36:47 +0430

    atoms are nat

--- Formula.v
+++ Formula.v
@@ -1,6 +1,5 @@
 Require Import Lang.
-Require Import Nat.
-Require Import AtomSet.
+Require Import Finset.
 
 Fixpoint weight (p : prop) : nat :=
 match p with
@@ -12,13 +11,64 @@ match p with
   | _ => S O
   end.
 
-Fixpoint atoms (p : prop) : atom_set :=
-match p with
-  | Var a => {{a}}
-  | p1 ∧ p2 => (atoms p1) ∪ (atoms p2)
-  | p1 ∨ p2 => (atoms p1) ∪ (atoms p2)
-  | p1 ⊃ p2 => (atoms p1) ∪ (atoms p2)
-  | p1 ⊂ p2 => (atoms p1) ∪ (atoms p2)
-  | p1 ⊗ p2 => (atoms p1) ∪ (atoms p2)
-  | _ => EMP
+Fixpoint atoms_of (p : prop) : finset :=
+  match p with
+  | ^x_a => {{a}}
+  | p1 ∧ p2 => (atoms_of p1) ∪ (atoms_of p2)
+  | p1 ∨ p2 => (atoms_of p1) ∪ (atoms_of p2)
+  | p1 ⊃ p2 => (atoms_of p1) ∪ (atoms_of p2)
+  | p1 ⊂ p2 => (atoms_of p1) ∪ (atoms_of p2)
+  | p1 ⊗ p2 => (atoms_of p1) ∪ (atoms_of p2)
+  | _ => ∅
   end.
+Notation "^V" := atoms_of.
+
+Fixpoint atoms_num (p : prop) : nat :=
+  match p with
+    | ^x__ => 1
+    | p1 ∧ p2 => (atoms_num p1) + (atoms_num p2)
+    | p1 ∨ p2 => (atoms_num p1) + (atoms_num p2)
+    | p1 ⊃ p2 => (atoms_num p1) + (atoms_num p2)
+    | p1 ⊂ p2 => (atoms_num p1) + (atoms_num p2)
+    | p1 ⊗ p2 => (atoms_num p1) + (atoms_num p2)
+    | _ => 0
+    end.
+(* 
+Require Import BinNat BinNatDef BinPos BinPosDef.
+Fixpoint partition {T : Type} (l : list T) (mask : N) : list T :=
+  match (l, mask) with
+  | (_, 0%N) => []
+  | ([], _) => []
+  | (h :: t, pos p) => match p with
+                      | xH => [h]
+                      | xO p => partition t p
+                      | xI p => h :: (partition t p)
+                      end
+  end.
+
+Fixpoint partition_list {T : Type} (l : list T) (mask : N) : list (list T) :=
+  match (l, mask) with
+  ([], _) => [[]]
+  (_, 0%N) => [[]]
+  (h :: t, pos p) => match p with
+                    | xH => [[h]]
+                    | xO p => 
+  end.
+
+Compute (partition_list [2;3;4;5]).
+
+
+Fixpoint E0 (a : atom) (G : list prop) (mask : nat) : prop :=
+  match G with
+  | [] => 1
+  | [^x_b] => if PeanoNat.Nat.eqb a b then ⊤ else ^x_b
+  | (p ∧ q) :: G => (E0 a (p :: G) mask) ∧ (E0 a (p :: G) mask)
+  | (p ∨ q) :: G => (E0 a (p :: G) mask) ∨ (E0 a (p :: G) mask)
+  | (p ⊗ q) :: G => E0 a (p :: q :: G) mask
+  | _ => 0
+  end.
+
+
+Fixpoint E1 (a : atom) (G : list prop) (mask : nat) : prop :=
+  match (BinNatDef.of_nat mask) with
+  | 0%N =>  *)
--- Lang.v
+++ Lang.v
@@ -1,4 +1,4 @@
-Parameter atom : Set.
+Definition atom := nat.
 
 Inductive prop : Type :=
 | Var : atom  -> prop
@@ -14,19 +14,21 @@ Inductive prop : Type :=
 
 Definition Neg (p : prop) : prop := LDiv p Bot.
 
+Notation "^x_ x" := (Var x) (at level 30).
+Coercion atom_as_prop (a : atom) : prop := Var a.
+
+
 Notation "⊥" := Bot. (* 22a5 *)
 Notation "⊤" := Top. (* 22a4 *)
 Notation "0" := Zero.
 Notation "1" := One.
-Notation "¬ P" := (Neg P) (at level 51). (* 00ac *)
-Infix "∧" := Conj (left associativity, at level 52). (* 2227 *)
-Infix "∨" := Disj (left associativity, at level 53). (* 2228 *)
-Infix "⊃" := LDiv (right associativity, at level 54). (* 2283 *)
-Infix "⊂" := RDiv (right associativity, at level 55). (* 2282 *)
-Infix "⊗" := MCon (right associativity, at level 56). (* 2297 *)
-
-Axiom atomeq_dec : forall a b : atom, {a = b} + {a <> b}.
-
+Notation "¬ P" := (Neg P) (at level 31). (* 00ac *)
+Infix "∧" := Conj (left associativity, at level 32). (* 2227 *)
+Infix "∨" := Disj (left associativity, at level 33). (* 2228 *)
+Infix "⊃" := LDiv (right associativity, at level 34). (* 2283 *)
+Infix "⊂" := RDiv (right associativity, at level 35). (* 2282 *)
+Infix "⊗" := MCon (right associativity, at level 36). (* 2297 *)
+Require Import PeanoNat.
 Theorem propeq_dec : forall p q : prop, {p = q} + {p <> q}.
 Proof.
   induction p; induction q;
@@ -35,7 +37,22 @@ Proof.
       try (destruct (IHp1 q1); destruct (IHp2 q2); subst;
         try (right; intros H; inversion H; contradiction n);
         left; reflexivity).
-    destruct (atomeq_dec a a0).
+    destruct (PeanoNat.Nat.eq_dec a a0).
       - left. subst. reflexivity.
       - right. intros H. inversion H. contradiction n.
 Qed.
+
+Fixpoint propeq (p q : prop) : bool :=
+  match (p, q) with
+  | (^x_n, ^x_m) => PeanoNat.Nat.eqb n m
+  | (⊥, ⊥) => true
+  | (⊤, ⊤) => true
+  | (0, 0) => true
+  | (1, 1) => true
+  | (p1 ∧ p2, q1 ∧ q2) => (propeq p1 q1) && (propeq p2 q2)
+  | (p1 ∨ p2, q1 ∨ q2) => (propeq p1 q1) && (propeq p2 q2)
+  | (p1 ⊃ p2, q1 ⊃ q2) => (propeq p1 q1) && (propeq p2 q2)
+  | (p1 ⊂ p2, q1 ⊂ q2) => (propeq p1 q1) && (propeq p2 q2)
+  | (p1 ⊗ p2, q1 ⊗ q2) => (propeq p1 q1) && (propeq p2 q2)
+  | _ => false
+  end.
